73eae749a8fa801077532c504c066908a26c4356
Gnieark <gnieark@free.fr>
2021-04-09 14:36:04 +0200

    document BackgroundPHPTask class

--- BackgroundPHPTasks.php
+++ BackgroundPHPTasks.php
@@ -2,15 +2,79 @@
 class BackgroundPHPTask
 {
   
+    /**
+     * The TplBlock class.
+     *
+     * @category Process
+     * @package  BackgroundPHPTask
+     * @author   gnieark <gnieark@tinad.fr>
+     * @license  GNU General Public License V3
+     * @link     https://github.com/gnieark/BackgroundPHPtasks
+     */
+
+    /**
+     * The full path of the pid file to use or create.
+     *
+     * @var string
+     */
 
     private $pidFile = "";
+
+    /**
+     * the pid id, will be populated after exec.
+     *
+     * @var int
+     */
+
     private $pid;
+
+    /**
+     * Where is stored the script output.
+     *
+     * @var string
+     */
+
     private $outputFile = "/dev/null";
+
+    /**
+     * The php's script to execute full path + name .
+     *
+     * @var string
+     */
+
     private $phpScript;
-    private $identifier =""; //not the PID, juste another identifier if needed
+
+    /**
+     * Args to pass to the script when called. /!\Shell arguments, not query args $_GET 
+     * 
+     * @var array
+     */
+
     private $args = array();
 
+
+    /**
+    * Store the status
+    * 
+    * @var string
+    */
+
+
     private $status = "pending"; //should be pending/running/terminated
+
+    /**
+     * Use it as title, unique-key, as you want
+     *
+     * @var string
+     */
+
+    private $identifier =""; //not the PID, juste another identifier if needed
+
+    /**
+     * Return the status, Eventually check for a change before
+     *
+     * @return string 
+     */
 
 
     public function get_status()
@@ -22,19 +86,45 @@ class BackgroundPHPTask
         return $this->status;
     }
 
+    /**
+     * Return the pid (normaly useless,for debug)
+     *
+     * @return integer
+     */
+
     public function get_pid()
     {
         return $this->pid;
     }
 
+    /**
+     * Return the pid file path (normaly useless,for debug)
+     *
+     * @return string
+     */
+ 
     public function get_pifFile()
     {
         return $this->pidFile;
     }
+
+    /**
+     * Return the php script file path (normaly useless,for debug)
+     *
+     * @return string
+     */
+
     public function get_phpScript()
     {
         return $this->phpScript;
     }
+
+    /**
+     * Return the last pid on a pid file
+     *
+     * @return integer
+     */
+
     private function get_pid_from_pidfile()
     {
         
@@ -42,11 +132,27 @@ class BackgroundPHPTask
         return intval( $data[count($data) -1] );
     }
 
+    /**
+     * Set the php script to use
+     *
+     * @return BackgroundPHPTask for chaining
+     * 
+     * @param $phpScript the path
+     */
+
     public function set_phpScript(string $phpScript)
     {
         $this->phpScript = $phpScript;
         return $this;
     }
+
+    /**
+     * Set the php script to use, but by given the whole script on a string
+     *
+     * @return BackgroundPHPTask for chaining
+     * 
+     * @param $script the script. containing the opening bracket <?php
+     */
 
     public function set_phpScriptWithoutFile(string $script)
     {
@@ -54,28 +160,69 @@ class BackgroundPHPTask
         file_put_contents($scriptPath, $script);
         return $this->set_phpScript($scriptPath);
     }
-
-    public function set_identifier($identifier)
+   /**
+     * Set an identifier (optional). Should be usefull for your Deus Ex
+     * Machina
+     * @return BackgroundPHPTask for chaining
+     * 
+     * @param string $identifier, what you want
+     */
+    public function set_identifier(string $identifier)
     {
         $this->identifier = $identifier;
         return $this;
     }
 
-    public function add_arg($arg){
+    /**
+     * add an argument to give to the script
+     * 
+     * @return BackgroundPHPTask for chaining
+     * 
+     * @param string $arg, what you want
+     */
+
+    public function add_arg(string $arg){
         $this->args[] = escapeshellarg($arg);
         return $this;
     }
 
-    public function set_outputFile($outputFile)
+    /**
+     * Set where your scripts outputs are going.
+     * If none given, default is /dev/null
+     * 
+     * @return BackgroundPHPTask for chaining
+     * 
+     * @param string Path of output file (will be created if not yet existing)
+     */
+
+    public function set_outputFile(string $outputFile)
     {
         $this->outputFile = $outputFile;
         return $this;
     }
-    public function set_pidFile($pidFile)
+
+    /**
+     * Set the pid file where the process id will be stored
+     * 
+     * 
+     * @return BackgroundPHPTask for chaining
+     * 
+     * @param string Path of pid file (will be created if not yet existing)
+     */
+
+    public function set_pidFile(string $pidFile)
     {
         $this->pidFile = $pidFile;
         return $this;
     }
+
+    /**
+     * Launch the script execution
+     * 
+     * @return BackgroundPHPTask for chaining
+     * 
+     */
+
 
     public function exec()
     {
@@ -90,6 +237,12 @@ class BackgroundPHPTask
         $this->pid = $this->get_pid_from_pidfile();
     }
 
+    /**
+     * test if a script is running (using his pid)
+     * 
+     * @return bool
+     */
+
     public function is_running()
     {
         try{
@@ -101,12 +254,26 @@ class BackgroundPHPTask
         return false;
     }
 
+    /**
+     * Kill the current process if running
+     * 
+     * @return BackgroundPHPTask for chaining
+     * 
+     */
+
     public function stop()
     {
         posix_kill( $this->pid, SIGTERM );
         return $this;
     }
 
+    /**
+     * Delete the current process output file, if you need to make cleaness
+     * 
+     * @return BackgroundPHPTask for chaining
+     * 
+     */
+    
     public function remove_output_file()
     {
         if(file_exists ( $this->outputFile )){
